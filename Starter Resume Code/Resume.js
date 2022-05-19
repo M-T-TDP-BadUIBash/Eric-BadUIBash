fe622a31da07d3a41621eac5f32e4237c255be54
Eric Liu <eliu4@buffalo.edu>
2022-05-19 09:59:59 -0400

    submission

---
+++ Starter Resume Code/Resume.js
@@ -0,0 +1,61 @@
+function getColorCode() {
+  var makeColorCode = "0123456789ABCDEF";
+
+  var code = "#";
+
+  for (var count = 0; count < 6; count++) {
+    code = code + makeColorCode[Math.floor(Math.random() * 16)];
+  }
+
+  return code;
+}
+
+let captchaVerified = false;
+
+let colorDiv = document.getElementById("color-captcha");
+
+let userText = document.querySelector("#textBox");
+
+let submitButton = document.querySelector("#submitButtons");
+
+let output = document.querySelector("#output");
+
+let refreshButton = document.querySelector("#refreshButton");
+
+let submitResume = document.querySelector("#submit");
+
+let captchaColorCode = getColorCode();
+
+colorDiv.style.backgroundColor = captchaColorCode;
+
+submitButton.addEventListener("click", function () {
+  if (userText.value === captchaColorCode) {
+    output.classList.add("correctCaptcha");
+
+    output.innerHTML = "Correct!";
+
+    captchaVerified = true;
+  } else {
+    output.classList.add("incorrectCaptcha");
+
+    output.innerHTML = "Incorrect, please try again";
+  }
+});
+
+refreshButton.addEventListener("click", function () {
+  userText.value = "";
+
+  output.innerHTML = "";
+
+  captchaColorCode = getColorCode();
+
+  colorDiv.style.backgroundColor = captchaColorCode;
+});
+
+submitResume.addEventListener("click", function () {
+  if (captchaVerified) {
+    window.location.href = "Submission.html";
+  } else {
+    alert("please complete captcha");
+  }
+});
